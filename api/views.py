792ff1dfe43f8263a6b38695b977d564c78edef7
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-12-08 12:52:48 +0100

    getList

--- api/views.py
+++ api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def getAll(request) :
-    return "Not implemented yet"
+    return HttpResponse("Skyrim\nGOT\nRequiem for a Dream\nZelda")
 
 def get(request) :
     return "Not implemented yet"

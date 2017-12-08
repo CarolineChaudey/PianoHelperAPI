3ddb2a25a73f319af2e20ca8e23f66be84277a14
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-12-08 14:28:31 +0100

    choose song

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.getAll, name='getAll')#,
-    #url(r'^(?[0-9]+)$', views.get, name='get')
+    url(r'^$', views.getAll, name='getAll'),
+    url(r'^(?P<music_id>[0-9]+)$', views.get, name='get')
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 
 def getAll(request) :
-    return HttpResponse("Skyrim\nGOT\nRequiem for a Dream\nZelda")
+    return HttpResponse("1) Skyrim\n2) GOT\n3) Requiem for a Dream\n4) Zelda")
 
-def get(request) :
-    return "Not implemented yet"
+def get(request, music_id) :
+    if music_id == "1":
+        return HttpResponse("Skyrim")
+    elif music_id == "2":
+        return HttpResponse("GOT")
+    elif music_id == "3":
+        return HttpResponse("Requiem for a Dream")
+    elif music_id == "4":
+        return HttpResponse("Zelda")
+    else:
+        return HttpResponse("No such song")

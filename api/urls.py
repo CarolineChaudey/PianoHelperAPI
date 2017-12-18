cc8f655ff5f125da7c1adc3ee01092691b5347e6
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-12-18 12:59:10 +0100

    get midi description

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.getAll, name='getAll'),
+    url(r'^$', views.get_all, name='getAll'),
     url(r'^(?P<music_id>[0-9]+)$', views.get, name='get')
 ]
--- api/views.py
+++ api/views.py
@@ -1,18 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+import midi
 
 
-def getAll(request) :
+def get_all(request) :
     return HttpResponse("1) Skyrim\n2) GOT\n3) Requiem for a Dream\n4) Zelda")
+
 
 def get(request, music_id) :
     if music_id == "1":
-        return HttpResponse("Skyrim")
+        return get_midi_pattern("api/sheets/Dragonborn-Skyrim.mid")
     elif music_id == "2":
-        return HttpResponse("GOT")
+        pattern = midi.read_midifile("api/sheets/GameofThrones.mid")
+        nbTracks = len(pattern.tracks)
+        return get_midi_pattern("Nb tracks = ", nbTracks)
     elif music_id == "3":
-        return HttpResponse("Requiem for a Dream")
+        return get_midi_pattern("api/sheets/rfd.mid")
     elif music_id == "4":
-        return HttpResponse("Zelda")
+        return get_midi_pattern("api/sheets/ThemeA.mid")
     else:
         return HttpResponse("No such song")
+
+
+def get_midi_pattern(path):
+    pattern = midi.read_midifile(path)
+    return HttpResponse(pattern)

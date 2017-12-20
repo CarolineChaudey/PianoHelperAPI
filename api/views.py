8446ff1b76cc30e0db1e827c6dd5adc4c39b644a
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-12-20 13:44:58 +0100

    added keyboard cat

--- api/views.py
+++ api/views.py
@@ -3,20 +3,26 @@ import midi
 
 
 def get_all(request) :
-    return HttpResponse("1) Skyrim\n2) GOT\n3) Requiem for a Dream\n4) Zelda")
+    return HttpResponse("1) Keyboard cat\n2) GOT\n3) Requiem for a Dream\n4) Zelda")
 
 
 def get(request, music_id) :
     if music_id == "1":
-        return get_midi_pattern("api/sheets/Dragonborn-Skyrim.mid")
+        pattern = midi.read_midifile("api/sheets/keyboard_cat.mid")
+        result = format_pattern(pattern)
+        return HttpResponse(result)
     elif music_id == "2":
         pattern = midi.read_midifile("api/sheets/GameofThrones.mid")
-        nbTracks = len(pattern.tracks)
-        return get_midi_pattern("Nb tracks = ", nbTracks)
+        result = format_pattern(pattern)
+        return HttpResponse(result)
     elif music_id == "3":
-        return get_midi_pattern("api/sheets/rfd.mid")
+        pattern = midi.read_midifile("api/sheets/rfd.mid")
+        result = format_pattern(pattern)
+        return HttpResponse(result)
     elif music_id == "4":
-        return get_midi_pattern("api/sheets/ThemeA.mid")
+        pattern = midi.read_midifile("api/sheets/ThemeA.mid")
+        result = format_pattern(pattern)
+        return HttpResponse(result)
     else:
         return HttpResponse("No such song")
 
@@ -24,3 +30,15 @@ def get(request, music_id) :
 def get_midi_pattern(path):
     pattern = midi.read_midifile(path)
     return HttpResponse(pattern)
+
+
+def format_pattern(pattern):
+    result = []
+    for track in pattern:
+        desc_track = []
+        for event in track:
+            event_type = event.__class__.__name__
+            if event_type == "NoteOffEvent" or event_type == "NoteOnEvent":
+                desc_track.append(event)
+        result.append(desc_track)
+    return result

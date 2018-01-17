e5ab2816341a50963c5f26e1e9aff165bb24026c
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2018-01-17 12:51:51 +0100

    convert midi notes

--- api/views.py
+++ api/views.py
@@ -33,8 +33,42 @@ def format_pattern(pattern):
         for event in track:
             event_type = event.__class__.__name__
             if event_type == "NoteOffEvent":
-                desc_track.append(['off', event.tick, event.data[0]])
+                #desc_track.append(['off', event.tick, event.data[0]])
+                print("");
             elif event_type == "NoteOnEvent":
-                desc_track.append(['on', event.tick, event.data[0]])
+                if event.data[1] == 0:
+                    print("");
+                    #desc_track.append(['off', event.tick, event.data[0]])
+                else:
+                    desc_track.append(['on', event.tick, convert_note(event.data[0])])
         result.append(desc_track)
     return result
+
+
+def convert_note(note_code):
+    if note_code in (24, 36, 48, 60, 72, 84, 96):
+        return 1
+    elif note_code in (26, 38, 50, 62, 74, 86, 98):
+        return 2
+    elif note_code in (28, 40, 52, 64, 76, 88, 100):
+        return 3
+    elif note_code in (29, 41, 53, 65, 77, 89, 101):
+        return 4
+    elif note_code in (31, 43, 55, 67, 79, 91, 103):
+        return 5
+    elif note_code in (33, 45, 57, 69, 81, 93, 105):
+        return 6
+    elif note_code in (35, 47, 59, 71, 83, 95, 107):
+        return 7
+    elif note_code in (25, 37, 49, 61, 73, 85, 97):
+        return 1.5
+    elif note_code in (27, 39, 51, 63, 75, 87, 99):
+        return 2.5
+    elif note_code in (30, 42, 54, 66, 78, 90, 102):
+        return 4.5
+    elif note_code in (32, 44, 56, 68, 80, 92, 104):
+        return 5.5
+    elif note_code in (34, 46, 58, 70, 82, 94, 106):
+        return 6.5
+    else:
+        return "wtf"
